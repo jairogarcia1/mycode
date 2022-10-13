6d08f5b215369a3611e9fd14a0adef76fae1108a
Mona Lisa <monalisa@users.noreply.github.com>
2022-10-13 20:24:36 +0000

    CHALLENGE- Code Repair!

---
+++ first_challenge/challenge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    name_input = input("Please enter your name:")
+    day_input = input("What day is it today?:")  
+
+    ## print() can be given a series of objects separated by a comma
+    print("Hello,", name_input, "! Happy ", day_input, "!")
+
+
+main()
+
