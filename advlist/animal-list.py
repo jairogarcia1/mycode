ff6a5a3e34b5b5ce4ea5750276dada2df7d0f2d8
Mona Lisa <monalisa@users.noreply.github.com>
2022-10-18 17:17:26 +0000

    python dictionaries

---
+++ advlist/animal-list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Jgarcia
+   List - OPTIONAL CHALLENGE"""
+
+list = ["Fox", "Fly", "Ant"], ["Bee", "Cod"], ["Cat", "Dog", "Yak", "Cow"], ["Hen", "Koi", "Hog", "Yak", "Ant"]
+
+print(list)
